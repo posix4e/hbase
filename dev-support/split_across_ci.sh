be81aeaf1ed7d150dc0959585c6ffdd0e54f280f
Alex Newman <posix4e@apache.org>
2014-09-14 19:04:29 -0700

    Turn on reuseforks

--- dev-support/split_across_ci.sh
+++ dev-support/split_across_ci.sh
@@ -3,21 +3,21 @@ set -ex
 
 i=0
 tests='echo starting'
-for test in "mvn test -PskipServerTests" \
-"mvn -pl hbase-server test -PrunVerySlowRegionServerTests" \
-"mvn -pl hbase-server test -PrunVerySlowMapReduceTests" \
-"mvn -pl hbase-server test -PrunRegionServerTests" \
-"mvn -pl hbase-server test -PrunMapreduceTests" \
-"mvn -pl hbase-server test -PrunMiscTests" \
-"mvn -pl hbase-server test -PrunClientTests" \
-"mvn -pl hbase-server test -PrunMasterTests" \
-"mvn -pl hbase-server test -PrunCoprocessorTests" \
-"mvn -pl hbase-server test -PrunFilterTests" \
-"mvn -pl hbase-server test -PrunIOTests" \
-"mvn -pl hbase-server test -PrunRPCTests" \
-"mvn -pl hbase-server test -PrunRestTests" \
-"mvn -pl hbase-server test -PrunReplicationTests" \
-"mvn -pl hbase-server test -PrunSecurityTests" \
+for test in "mvn -DreuseForks=true test -PskipServerTests" \
+"mvn -DreuseForks=true -pl hbase-server test -PrunVerySlowRegionServerTests" \
+"mvn -DreuseForks=true -pl hbase-server test -PrunVerySlowMapReduceTests" \
+"mvn -DreuseForks=true -pl hbase-server test -PrunRegionServerTests" \
+"mvn -DreuseForks=true -pl hbase-server test -PrunMapreduceTests" \
+"mvn -DreuseForks=true -pl hbase-server test -PrunMiscTests" \
+"mvn -DreuseForks=true -pl hbase-server test -PrunClientTests" \
+"mvn -DreuseForks=true -pl hbase-server test -PrunMasterTests" \
+"mvn -DreuseForks=true -pl hbase-server test -PrunCoprocessorTests" \
+"mvn -DreuseForks=true -pl hbase-server test -PrunFilterTests" \
+"mvn -DreuseForks=true -pl hbase-server test -PrunIOTests" \
+"mvn -DreuseForks=true -pl hbase-server test -PrunRPCTests" \
+"mvn -DreuseForks=true -pl hbase-server test -PrunRestTests" \
+"mvn -DreuseForks=true -pl hbase-server test -PrunReplicationTests" \
+"mvn -DreuseForks=true -pl hbase-server test -PrunSecurityTests" \
 
 do
   if [ $(($i % $CIRCLE_NODE_TOTAL)) -eq $CIRCLE_NODE_INDEX ]
